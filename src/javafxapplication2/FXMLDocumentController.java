bffde6df5b50096636b6a5c4d45d61ad7d997e35
Joonhee <pakjoonhee90@gmail.com>
2018-11-21 19:30:28 -0800

    first commit

---
+++ src/javafxapplication2/FXMLDocumentController.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapplication2;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author pakjo
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    public void changeScreenAddPart(ActionEvent event) throws IOException 
+    {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("AddPartScreen.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+        
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(tableViewScene);
+        window.show();
+    }
+    
+    public void changeScreenModifyPart(ActionEvent event) throws IOException 
+    {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("AddPartScreen.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+        
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(tableViewScene);
+        window.show();
+    }
+    
+    public void changeScreenDeletePart(ActionEvent event) throws IOException 
+    {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("AddPartScreen.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+        
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(tableViewScene);
+        window.show();
+    }
+    
+    public void changeScreenAddProduct(ActionEvent event) throws IOException 
+    {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("AddPartScreen.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+        
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(tableViewScene);
+        window.show();
+    }
+    
+    public void changeScreenModifyProduct(ActionEvent event) throws IOException 
+    {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("AddPartScreen.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+        
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(tableViewScene);
+        window.show();
+    }
+    
+    public void changeScreenDeleteProduct(ActionEvent event) throws IOException 
+    {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("AddPartScreen.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+        
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(tableViewScene);
+        window.show();
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
